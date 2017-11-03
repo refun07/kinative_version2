4bcf37dc51ed823d2f4e093d91939eda61d16c83
Refun <refun@kinative.com>
2017-11-03 12:14:30 +0600

    changed header

--- single-blog.php
+++ single-blog.php
@@ -1,5 +1,75 @@
-<?php include('inc/header.php'); ?>
+<?php include('inc/header.php'); ?><!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />  
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Kinative-WE BUILD YOUR BRAND</title>
+<link href="libraries/bootstrap/bootstrap.min.css" rel="stylesheet"/>
+  
 
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css'>
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.0/assets/owl.theme.default.min.css'>
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.0/assets/owl.carousel.min.css'>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/air-datepicker/2.2.3/css/datepicker.min.css"/>
+<link rel="stylesheet" href="libraries/magnific/magnific-popup.css">
+<link rel="stylesheet" href="libraries/hover-min.css"/>
+
+<!-- Core Owl Carousel CSS File  *  v1.3.3 -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet"/>
+<link rel="stylesheet" type="text/css" media="all"
+href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/smoothness/jquery-ui.css"    />
+
+
+<link href="css/component.css" rel="stylesheet"/>
+<link href="css/header.css" rel="stylesheet"/>
+<link href="style.css" rel="stylesheet"/>
+
+<link href="css/media.css" rel="stylesheet"/>
+ <!-- <script data-main="scripts/main" src="libraries/require.js"></script> -->
+</head>
+
+<body>
+   <!--  <div class="hameid-loader-overlay"></div> -->
+<!--
+********************************************************************
+* HEADER SECTION
+********************************************************************
+-->
+<section id="header" class="header">
+<div class="bg">
+<div class="navbar navbar-inverse navbar-fixed-top opaque-navbar">
+<div class="container">
+  
+  <div class="row">
+    
+  
+    <div class="navbar-header">
+     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navMain">
+                    <span class="sr-only">Toggle navigation</span>  
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+        </button>
+      <a class="navbar-brand" href="#"><img src="images/logo.png" alt="logo of kinative"/></a>
+    </div>
+    <div class="collapse navbar-collapse" id="navMain">
+      <ul class="nav navbar-nav pull-right">
+        <li ><a class="active hvr-underline-from-left" href="#">Home</a></li>
+        <li><a class="hvr-underline-from-left" href="#why">About</a></li>
+        <li><a class="hvr-underline-from-left" href="#services">Services</a></li> 
+        <li><a class="hvr-underline-from-left" href="#portfolios">Portfolio</a></li> 
+        <li><a class="hvr-underline-from-left" href="#features">Features</a></li> 
+          <li><a class="hvr-underline-from-left" href="#blog">Blog</a></li> 
+        <li><a   class="hvr-underline-from-left" href="#hello">Contact</a></li> 
+      </ul>
+    </div>
+  </div>
+  </div>
+</div>
 
 
 
